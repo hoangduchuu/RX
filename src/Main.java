0f36242592c8e0850afc093f31069b771b9fa337
Hoang Huu <hoangduchuuvn@gmail.com>
2018-06-08 02:26:51 +0700

    interval(),

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,16 @@
-import rx.Observable;
-import rx.observables.ConnectableObservable;
+import java.util.concurrent.TimeUnit;
 
-;
+
+;import rx.Observable;
 
 public class Main {
 
     public static void main(String[] args) {
-        ConnectableObservable<String> source =
-                Observable.just("Alpha","Beta","Gamma","Delta","Epsilon")
-                       .publish();
-//Set up observer 1
-        source.subscribe(s -> System.out.println("Observer 1: " + s));
-//Set up observer 2
-        source.map(String::length)
-                .subscribe(i -> System.out.println("Observer 2: " + i));
-
-        // need call connect function to open coonect
-        source.connect();
-
-//Fire!
+        Observable.interval(1, 1, TimeUnit.SECONDS)
+                .map(x -> x * 2)
+                .toBlocking()
+                .subscribe(s -> System.out.println(s+ " Mississippi"));
     }
-
 
     public static void sleep(long millis) {
         try {
