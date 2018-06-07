0e8578ee99c403ce2a90967a62e2f051e27d2140
Hoang Huu <hoangduchuuvn@gmail.com>
2018-06-07 20:07:24 +0700

    simple observable

--- src/Main.java
+++ src/Main.java
@@ -9,41 +9,72 @@ import rx.schedulers.Schedulers;
 public class Main {
 
     public static void main(String[] args) {
-        Observable<Long> secondIntervals =
-                Observable
-                        .interval(1, TimeUnit.MILLISECONDS)
-                .take(200)
-                .map(x->x*2);
-
-        secondIntervals
-                .subscribeOn(Schedulers.computation())
-                .observeOn(Schedulers.io())
-                .toBlocking()
-                .subscribe(new Observer<Long>() {
-                    @Override
-                    public void onCompleted() {
-                        System.out.print("abc " + "onCompleted");
-
-                    }
-
-                    @Override
-                    public void onError(Throwable throwable) {
-                        System.out.print("abc " + "onError");
-
-                    }
-
-                    @Override
-                    public void onNext(Long aLong) {
-                        System.out.println("abc " + aLong);
-                    }
+        // the onNext
+        Observable<Integer> source =
+                Observable.create(myEmmitter -> {
+                    myEmmitter.onNext(1);
+                    sleep(1000);
+                    myEmmitter.onNext(2);
+                    sleep(1000);
+                    myEmmitter.onNext(3);
+                    sleep(1000);
+                    myEmmitter.onNext(4);
+                    sleep(1000);
+                    myEmmitter.onNext(5);
+                    sleep(1000);
+                    myEmmitter.onCompleted();
                 });
+        System.out.println("ok");
+        source.subscribe(s -> System.out.println("RECEIVED: " + s));
 
 
+        // the onError
+        Observable<String> sourceErrors = Observable.create(emitter -> {
+            try {
+                emitter.onNext("Alpha");
+                emitter.onNext("Beta");
+                emitter.onNext("Gamma");
+                emitter.onNext("Delta");
+                emitter.onNext("Epsilon");
+                emitter.onCompleted();
+            } catch (Throwable e) {
+                emitter.onError(e);
+            }
+        });
+        sourceErrors
+                .subscribe(s -> System.out.println("Mysourcez: " + s),
+                        (Throwable throwable) -> {
+                            throwable.printStackTrace();
+                            System.out.println("Mysourcez error: " + throwable.getLocalizedMessage());
+
+                        });
+
+
+        ///source test filter
+        Observable<String> sourceFillterz = Observable.create(emitter -> {
+
+            try {
+                emitter.onNext("Alpha");
+                emitter.onNext("Beta");
+                emitter.onNext("Gamma");
+                emitter.onNext("Delta");
+                emitter.onNext("Epsilon");
+                emitter.onNext("a");
+                emitter.onNext("v");
+                emitter.onCompleted();
+            } catch (Throwable e) {
+                emitter.onError(e);
+            }
+        });
+        Observable<Integer> myLength = sourceFillterz.map(String::length);
+        Observable<Integer> filtered = myLength.filter(i -> i >= 2);
+        filtered.subscribe(s -> System.out.println("testfillter : " + s));
     }
+
 
     public static void sleep(long millis) {
         try {
-            Thread.sleep(millis);
+            Thread.sleep(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
---
+++ src/MyLog.java
@@ -0,0 +1,16 @@
+//import com.arcao.slf4j.timber.TimberLoggerFactory;
+//
+//import java.util.Timer;
+//import java.util.logging.Logger;
+//
+///**
+// * Created by hoang on 6/7/18 nhe.
+// */
+//class MyLog {
+//    public static void e(String... strings) {
+//sy
+//
+//
+//    }
+//
+//}
