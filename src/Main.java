b08ccbe58b7392cf54bfcb0c2167895056b8840e
Hoang Huu <hoangduchuuvn@gmail.com>
2018-06-08 03:43:02 +0700

    Single, Maybe, complete and what diffferencts?

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,31 @@
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
 import io.reactivex.Maybe;
 import io.reactivex.MaybeObserver;
 import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
 import io.reactivex.disposables.Disposable;
-
+import io.reactivex.functions.Consumer;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        Observable<String> source =
-                Observable.just("Alpha", "Beta", "Gamma");
-        source.first("abc") //returns a Single
-                .subscribe(x -> System.out.println(x), throwable -> System.out.println("" + throwable.getMessage()));
-
-        Maybe<Integer> intSource = Maybe.just(1);
-        intSource.subscribe(new MaybeObserver<Integer>() {
+        Single<String> source =
+                Single.just("Beta");
+        source.subscribe(new SingleObserver<String>() {
             @Override
             public void onSubscribe(Disposable disposable) {
+                System.out.println("onSubscribe 1");
 
             }
 
             @Override
-            public void onSuccess(Integer integer) {
+            public void onSuccess(String s) {
+                System.out.println("onSuccess : " + s);
 
             }
 
@@ -31,12 +33,57 @@ public class Main {
             public void onError(Throwable throwable) {
 
             }
+        });
+
+
+        //maybe
+        Maybe<String> intSource = Maybe.just("z");
+        intSource.subscribe(new MaybeObserver<String>() {
+            @Override
+            public void onSubscribe(Disposable disposable) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onSuccess(String integer) {
+                System.out.println("onSuccess : " + integer);
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError : ");
+
+            }
 
             @Override
             public void onComplete() {
+                System.out.println("onComplete : ");
 
             }
         });
+
+
+        // compllete able
+        Completable.complete()
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(Disposable disposable) {
+                        System.out.println("complete:  onsubcribe");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("complete:  onComplete with no data");
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        System.out.println("complete:  onError with no data");
+
+                    }
+                });
 
     }
 
