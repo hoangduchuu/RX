923b2874d068c251424cdeb1d59dd31e5089441a
Hoang Huu <hoangduchuuvn@gmail.com>
2018-06-08 12:58:32 +0700

    Disposable class, help we stop, break observable enytime we need
    s

--- src/Main.java
+++ src/Main.java
@@ -17,18 +17,26 @@ public class Main {
         CountDownLatch latch = new CountDownLatch(1000);
 
         Observable
-                .interval(1, 1, TimeUnit.MILLISECONDS)
+                .interval(1, 1, TimeUnit.SECONDS)
                 .take(100)
                 .subscribe(new Observer<Long>() {
+                    private Disposable disposable;
+
                     @Override
                     public void onSubscribe(Disposable disposable) {
+                        this.disposable = disposable;
+
                         System.out.println("onSubscribe");
+
                     }
 
                     @Override
                     public void onNext(Long aLong) {
                         latch.countDown();
                         System.out.println("onNext along: " + aLong + "--cound: - " + latch.getCount());
+                        if (aLong == 2){
+                            disposable.dispose();
+                        }
 
                     }
 
